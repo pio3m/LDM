fb5ee534e7010c6e653fa5e806b0f72c551c392c
piotr <p@wp.pl>
2025-03-11 18:20:53 +0100

    fix server

--- backend.py
+++ backend.py
@@ -96,4 +96,4 @@ def process():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
