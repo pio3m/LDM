6d2eb4bd6d6221ec5197a241ccb7ee7f0c15d98a
Piotr Dul <piotr.firebrigade@gmail.com>
2025-06-05 20:22:12 +0200

    add backup

--- copy.sh
+++ copy.sh
@@ -8,6 +8,11 @@ REMOTE_USER="root"
 REMOTE_HOST="161.35.214.6"
 REMOTE_DIR="/srv"
 
+# Data i godzina do nazwy backupu
+BACKUP_DATE=$(date +"%Y%m%d_%H%M%S")
+# Tworzenie backupu na serwerze (tar)
+ssh "$REMOTE_USER@$REMOTE_HOST" "cd $REMOTE_DIR && tar -czf backup_${BACKUP_DATE}.tar.gz ./*"
+
 # Kopiowanie zawartości katalogu za pomocą scp
 scp -r "$LOCAL_DIR"/* "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
 
